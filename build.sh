4ab92b926214c56b6b7360c1eff3c8b21e4fa262
mrlee23 <mrlee_23@naver.com>
2017-12-28 12:24:05 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ git pull
 mv ../.published/* ./
 git add -f .
 git commit -a -m "Deploy: $(git log --oneline -n 1)"
-test $? -eq "0" && git push origin gh-pages
+test $? -eq "0" && git remote add origin/pages && https://${G}@github.com/mrlee23/github-test.git && git push origin/gh-pages
 git checkout master
