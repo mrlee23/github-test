765c61786a02addf3dd11d95cf5f033322265a0f
mrlee23 <mrlee_23@naver.com>
2017-12-28 11:45:23 +0900

    Update gh-pages builder.

--- build.sh
+++ build.sh
@@ -1,2 +1,9 @@
-cd ../
-ls
+rm -rf ../.published
+npm run doc
+git checkout gh-pages
+git pull
+mv ../.published/* ./
+git add -f .
+git commit -a -m "Deploy: $(git log --oneline -n 1)"
+test $? -eq "0" && git push origin gh-pages --quiet
+git checkout master
