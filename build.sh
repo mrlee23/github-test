dc29705e97b5b2e9ab0799bfd1c38a2552224c30
mrlee23 <mrlee_23@naver.com>
2017-12-29 20:43:07 +0900

    Test

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@ git config --global user.email "mrlee_23@naver.com"
 git config --global user.name "mrlee23"
 rm -rf ../.published
 npm run doc
+ls ../
+echo "A"
 git checkout --orphan gh-pages
 git rm -rf .
 git pull origin gh-pages
