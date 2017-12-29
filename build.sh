f60de1987baa1535837b31a320d180f513bfc80e
mrlee23 <mrlee_23@naver.com>
2017-12-29 20:28:21 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,10 +2,11 @@ git config --global user.email "mrlee_23@naver.com"
 git config --global user.name "mrlee23"
 rm -rf ../.published
 npm run doc
-git checkout -b gh-pages
-git pull
-mv ../.published/* ./
+git checkout --orphan gh-pages
+git rm -rf .
+git pull origin gh-pages
+mv -f ../.published/* ./
 git add -f .
 git commit -a -m "Deploy: $(git log --oneline -n 1)"
-test $? -eq "0" && git remote add origin/pages && https://${G}@github.com/mrlee23/github-test.git && git push origin/gh-pages
+test $? -eq "0" && git push origin gh-pages
 git checkout master
