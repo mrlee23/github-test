7686d0be3712d61078c934af7481275d40d68b1d
mrlee23 <mrlee_23@naver.com>
2017-12-29 20:52:34 +0900

    Changed build

--- build.sh
+++ build.sh
@@ -1,18 +1,12 @@
 git config --global user.email "mrlee_23@naver.com"
 git config --global user.name "mrlee23"
-rm -rf ../.published
+rm -rf ./.published
 npm run doc
-ls -alh ../
-echo "A"
 git checkout --orphan gh-pages
-ls -alh 
 git rm -rf .
-ls -alh
 git pull origin gh-pages
-ls -alh
-mv -f ../.published/* ./
-ls -alh
-git add .
+git add ./.published
+git mv -f ./.published/* ./
 git commit -a -m "Deploy: $(git log --oneline -n 1)"
 test $? -eq "0" && git push origin gh-pages
 git checkout master
